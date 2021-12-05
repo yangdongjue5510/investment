73a0421bb249466316293be91df756c375442280
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-06 03:47:01 +0900

    fix: add toString in Date type

--- src/test/java/com/fastcampus/investment/api/ApisTest.java
+++ src/test/java/com/fastcampus/investment/api/ApisTest.java
@@ -46,15 +46,15 @@ class ApisTest {
                 .andExpect(jsonPath("$.data[0].totalInvestAmount", is(100000000)))
                 .andExpect(jsonPath("$.data[0].investedCount", is(0)))
                 .andExpect(jsonPath("$.data[0].investedAmount", is(0)))
-                .andExpect(jsonPath("$.data[0].startedAt", is(LocalDate.now().minusDays(1L))))
-                .andExpect(jsonPath("$.data[0].finishedAt", is(LocalDate.now().plusDays(1L))))
-                .andExpect(jsonPath("$.data[1].id", is(1)))
+                .andExpect(jsonPath("$.data[0].startedAt", is(LocalDate.now().minusDays(1L).toString())))
+                .andExpect(jsonPath("$.data[0].finishedAt", is(LocalDate.now().plusDays(1L).toString())))
+                .andExpect(jsonPath("$.data[1].id", is(4)))
                 .andExpect(jsonPath("$.data[1].title", is("TOBE-RICH of Warren Buffett")))
                 .andExpect(jsonPath("$.data[1].totalInvestAmount", is(600000000)))
                 .andExpect(jsonPath("$.data[1].investedCount", is(0)))
                 .andExpect(jsonPath("$.data[1].investedAmount", is(0)))
-                .andExpect(jsonPath("$.data[1].startedAt", is(LocalDate.now().minusDays(2L))))
-                .andExpect(jsonPath("$.data[1].finishedAt", is(LocalDate.now().plusDays(2L))))
+                .andExpect(jsonPath("$.data[1].startedAt", is(LocalDate.now().minusDays(2L).toString())))
+                .andExpect(jsonPath("$.data[1].finishedAt", is(LocalDate.now().plusDays(2L).toString())))
         ;
     }
 
